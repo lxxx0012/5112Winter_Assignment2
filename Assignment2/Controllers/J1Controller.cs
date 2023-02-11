92598e161a9f51fdadfc13618359e4ba33863196
lxxx0012 <alexyeeca@gmail.com>
2023-02-11 13:56:39 -0500

    Compute for J1 solution

--- Assignment2/Controllers/J1Controller.cs
+++ Assignment2/Controllers/J1Controller.cs
@@ -9,5 +9,21 @@ namespace Assignment2.Controllers
 {
     public class J1Controller : ApiController
     {
+        // Write a program that will compute the totla calories fo the meal
+        // GET : http://localhost/api/J1
+        // Compute {burger} - integer representing the index burger choice
+        // Compute {drink} - integer representing the index drink choice
+        // Compute {side} - Integer representing the index side choice
+        // Compute {dessert) - Integer representing the index dessert choice
+
+        [HttpGet]
+        [Route("api/Assignment2/J1/")]
+        public string menu (int B, int S, int D, int D)
+        {
+           if (B = 1)
+            {
+
+            }
+        }
     }
 }
