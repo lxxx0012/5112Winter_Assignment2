520a172274610663b6d93fa0d9bfb3ada958aa1b
lxxx0012 <alexyeeca@gmail.com>
2023-02-11 17:49:37 -0500

    compute 0/0/0/0

--- Assignment2/Controllers/J1Controller.cs
+++ Assignment2/Controllers/J1Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
 
 namespace Assignment2.Controllers
@@ -10,20 +11,105 @@ namespace Assignment2.Controllers
     public class J1Controller : ApiController
     {
         // Write a program that will compute the totla calories fo the meal
-        // GET : http://localhost/api/J1
-        // Compute {burger} - integer representing the index burger choice
-        // Compute {drink} - integer representing the index drink choice
-        // Compute {side} - Integer representing the index side choice
-        // Compute {dessert) - Integer representing the index dessert choice
+        /// <summary>
+        /// Compute the total calories for the meal
+        /// </summary>
+        /// <param name="Bu">Compute {burger} - integer representing the index burger choice</param>
+        /// <param name="Sd">Compute {side} - Integer representing the index side choice</param>
+        /// <param name="Dr">Compute {drink} - integer representing the index drink choice</param>
+        /// <param name="De">Compute {dessert) - Integer representing the index dessert choice</param>
+        /// <returns>
+        /// HTTP request of 4/4/4/4 -> 0 calories
+        /// HTTP request of 1/2/3/4 -> 691 calories
+        /// </returns>
 
         [HttpGet]
-        [Route("api/Assignment2/J1/")]
-        public string menu (int B, int S, int D, int D)
-        {
-           if (B = 1)
-            {
+        [Route("api/Assignment2/Menu/{Bu}/{Dr}/{Sd}/{De}")]
 
+
+        public string Menu(int Bu, int Dr, int Sd, int De)
+        {
+            var Burger = Bu;
+            var Drink = Dr;
+            var SideOrder = Sd;
+            var Dessert = De;
+
+            int BurgerCalories = 0;
+            if (Burger == 1)
+            {
+                BurgerCalories = 461;
             }
-        }
+            else if (Burger == 2)
+            {
+                BurgerCalories = 431;
+            }
+            else if (Burger == 3)
+            {
+                BurgerCalories = 420;
+    }
+            else if (Burger == 4) 
+            {
+                BurgerCalories = 0;
+            }
+
+            int DrinkCalories = 0;
+            if (Drink == 1)
+            {
+                DrinkCalories = 130;
+            }
+            else if (Drink == 2)
+            {
+                DrinkCalories = 160;
+            }
+            else if (Drink == 3)
+            {
+                DrinkCalories = 118;
+            }
+            else if (Drink == 4)
+            {
+                DrinkCalories = 0;
+            }
+
+            int SideOrderCalories = 0;
+            if (SideOrder == 1)
+            {
+                SideOrderCalories = 100;
+            }
+            else if (SideOrder == 2)
+            {
+                SideOrderCalories = 57;
+            }
+            else if (SideOrder == 3)
+            {
+                SideOrderCalories = 70;
+            }
+            else if (SideOrder == 4)
+            {
+                SideOrderCalories = 0;
+            }
+
+            int DessertCalories = 0;
+            if (Dessert == 1)
+            {
+                DessertCalories = 167;
+            } 
+            else if (Dessert == 2)
+            {
+                DessertCalories = 266;
+            } 
+            else if (Dessert == 3)
+            {
+                DessertCalories = 75;
+            } 
+            else if (Dessert == 4)
+            {
+                DessertCalories = 0;
+            }
+
+            int TotalCalories = BurgerCalories + SideOrderCalories + DrinkCalories + DessertCalories;
+            string message = "Your total calodries count is " + TotalCalories;
+            return message;
+        }           
+            
     }
 }
